200b2dd37817dcd17cf561a8099a3161b40f3ef9
clauHenrique <johnhenriqueoliveira2021@gmail.com>
2023-12-26 14:21:42 -0300

    alterar colecao filmes

--- backend/models/filmes.js
+++ backend/models/filmes.js
@@ -4,8 +4,8 @@ const filmeSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     atoresPrincipais: [String],
     genero: String,
-    descricao: String,
-    favorito: { type: Boolean, default: false }
+    sinopse: String,
+    imagem: { type: String, require: true }
 });
 
 module.exports = mongoose.model('Filme', filmeSchema);
